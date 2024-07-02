c53b2431153f30c6bd6866e35c2d3183269f1f21
Tatjana S <tatjana.stojanovic>
2024-07-02 17:03:42 +0200

    Added Police

--- Beach.java
+++ Beach.java
@@ -17,5 +17,6 @@ public class Beach extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(15, 10, 60); 
+        this.addObject(new Police(), 0, 0);
     }
 }
---
+++ Police.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Police here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Police extends Actor
+{
+    /**
+     * Act - do whatever the Police wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        if(Greenfoot.isKeyDown("space")){
+            this.move(1);
+        }
+        
+    }
+}
